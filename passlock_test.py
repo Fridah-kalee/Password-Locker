2aa1553e2416197da07479a75a65990a5b6080ab
Fridah-kalee <fridahkalee@gmail.com>
2022-04-23 12:24:59 +0300

    Imported unittest and User

---
+++ passlock_test.py
@@ -0,0 +1,9 @@
+import unittest
+from passlock import User
+
+class TestClass (unittest.TestCase):
+    '''
+    Test class that defines test cases for the user class behaviours.
+    Args:
+    unittest.TestCase:TestCase class that helps in creating test class
+    '''
