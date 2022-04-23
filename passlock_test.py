5a6e411e40d1e901cfec0c2ac181679891de57d8
Fridah-kalee <fridahkalee@gmail.com>
2022-04-23 12:40:06 +0300

    Testing if our objects are instantiated correctly

--- passlock_test.py
+++ passlock_test.py
@@ -7,3 +7,18 @@ class TestClass (unittest.TestCase):
     Args:
     unittest.TestCase:TestCase class that helps in creating test class
     '''
+    def setUp(self):
+        '''
+        Set up method that runs before each test case for individual
+        '''
+        self.new_user = User('FridahKalee', 'Kate@16z')
+
+    def test_init(self):
+        '''
+        A test case method to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_user.username,'FridahKalee')
+        self.assertEqual(self.new_user.password,'Kate@16z')
+
+if __name__ == '__main__':
+    unittest.main()
