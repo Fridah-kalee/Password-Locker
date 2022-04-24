3be23693b4e11cb56ff83d0f469ad05895936c03
Fridah-kalee <fridahkalee@gmail.com>
2022-04-24 10:41:10 +0300

    created functions that implement the behaviors we just created

--- run.py
+++ run.py
@@ -8,4 +8,23 @@ def create_new_user(username,password):
     new_user = User(username,password)
     return new_user
 
-    
+def save_user(user):
+    '''
+    Function to save user
+    '''
+    user.save_user()
+
+def display_user():
+    '''
+    Function to display existing user
+    '''
+    return User.display_user()
+
+def login_user(username,password):
+    '''
+    Function that checks whether a user exist and then login the user in
+    '''
+    checck_user = Credentials.verify_user(username,password)
+    return check_user
+
+def create_new_credenti                
